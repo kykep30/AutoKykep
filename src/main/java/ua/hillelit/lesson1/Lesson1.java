6b6664f487cb64ba30c3685d9adc97ff1618d4b7
kykep30 <prakof30@gmail.com>
2023-12-09 14:05:32 +0200

    kyk

--- src/main/java/ua/hillelit/lesson1/Lesson1.java
+++ src/main/java/ua/hillelit/lesson1/Lesson1.java
@@ -2,6 +2,15 @@ package ua.hillelit.lesson1;
 
 public class Lesson1 {
     public static void main(String[] args) {
+//        System.out.println();
+//
+//        byte b = 127;
+//        System.out.println("b=" + b);
+
+        char c = 1082;
+        System.out.println(c);
+
+
 
     }
 }
