ee14549cf144c411cb06c592a3f1e45e989410f7
kykep30 <prakof30@gmail.com>
2023-12-10 11:04:12 +0200

    kyk

---
+++ src/main/java/ua/hillelit/lesson1/HomeWorkLesson2.java
@@ -0,0 +1,40 @@
+package ua.hillelit.lesson1;
+
+import java.util.Scanner;
+
+public class HomeWorkLesson2 {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Введіть число: ");
+//
+//        int number = scanner.nextInt();
+//
+//        String message = (number %2 != 0) ? "Число є непарним." : "Число є парним.";
+//
+//        System.out.println(message);
+//
+//        scanner.close();
+
+
+//            double R = 10.5;
+//            double H = 15;
+//
+//            double S = Math.PI * R * R;
+//            double V = S * H;
+//        System.out.println(S);
+//        System.out.println(V);
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово: ");
+
+        String slovo = scanner.nextLine();
+
+        System.out.print("Ты ввел слово: " + slovo);
+
+
+    }
+}
+
+
