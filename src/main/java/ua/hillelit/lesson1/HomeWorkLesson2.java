b895bf5707541a2aa31bfb3f74413fb7747095ea
kykep30 <prakof30@gmail.com>
2023-12-16 20:53:25 +0200

    kyk

--- src/main/java/ua/hillelit/lesson1/HomeWorkLesson2.java
+++ src/main/java/ua/hillelit/lesson1/HomeWorkLesson2.java
@@ -4,34 +4,25 @@ import java.util.Scanner;
 
 public class HomeWorkLesson2 {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Введіть число: ");
-//
-//        int number = scanner.nextInt();
-//
-//        String message = (number %2 != 0) ? "Число є непарним." : "Число є парним.";
-//
-//        System.out.println(message);
-//
-//        scanner.close();
+
+//         Task 1
+        System.out.println("hello kykep");
+
+//         Task 2
+        double Celsius = 24;
+        double Fahrenheit = Celsius * 9/5 +32;
+        double Kelvin = Celsius + 273.16;
+
+        System.out.println(Celsius + " Celsius " + Fahrenheit + " Fahrenheit " + Kelvin + " Kelvin ");
+
+//         Task 3
+        int a = 4;
+        int b = 21;
+        int S = a * b;
+
+        System.out.println(S);
 
 
-//            double R = 10.5;
-//            double H = 15;
-//
-//            double S = Math.PI * R * R;
-//            double V = S * H;
-//        System.out.println(S);
-//        System.out.println(V);
-
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите слово: ");
-
-        String slovo = scanner.nextLine();
-
-        System.out.print("Ты ввел слово: " + slovo);
 
 
     }
