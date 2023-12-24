7e160bcb26bc0ab8e873bdab78e791ab96008bda
kykep30 <prakof30@gmail.com>
2023-12-24 09:20:32 +0200

    Init commit second

---
+++ src/main/homework6/Lesson6.java
@@ -0,0 +1,12 @@
+public class Lesson6 {
+    public static void main(String[] args) {
+//        KISS - keep it short and simple
+//        DRY - Don't Repeat Yourself (Не повторяйся)
+//        AGNI - You are not gone need it (Вам это не понадобиться)
+
+        String intrt = "ggggg asdf";
+
+        intrt.substring(6)
+
+    }
+}
